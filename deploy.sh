5f17798f36565885dada03fb10d63331636f9167
Tsukada <seiya.tsukada@accenture.com>
2019-05-19 19:54:40 +0900

    push

--- deploy.sh
+++ deploy.sh
@@ -7,16 +7,31 @@ curl http://${API_SERVER}
 
 sleep 10
 
-# get nodeport
-NODEPORT=`curl http://${API_SERVER}/nodeport`
+# get elasticsearch nodeport
+ES_NODEPORT=`curl http://${API_SERVER}/es/nodeport`
 
-echo ${NODEPORT}
+echo ${ES_NODEPORT}
 
 # request kibana
 echo ${KUBE_WORKER}
-echo ${NODEPORT}
-curl ${KUBE_WORKER}:${NODEPORT}
-ANS = `curl ${KUBE_WORKER}:${NODEPORT}`
+echo ${ES_NODEPORT}
+curl ${KUBE_WORKER}:${ES_NODEPORT}
+ANS = `curl ${KUBE_WORKER}:${ES_NODEPORT}`
 
 echo ${ANS}
+
+
+
+
+
+# get kibana nodeport
+
+KIBANA_NODEPORT=`curl http://${API_SERVER}/kibana/nodeport`
+
+echo ${KIBANA_NODEPORT}
+
+# request kibana
+echo ${KUBE_WORKER}
+echo ${KIBANA_NODEPORT}
+echo "request url : ${KUBE_WORKER}:${KIBANA_NODEPORT}"
 
