b552b897d2124063285b4944baed2426c595ed13
Tsukada <seiya.tsukada@accenture.com>
2019-05-19 19:12:34 +0900

    push

--- deploy.sh
+++ deploy.sh
@@ -2,5 +2,18 @@
 
 set -x
 
-# request
+# request deploy api
 curl http://${API_SERVER}
+
+sleep 10
+
+# get nodeport
+NODEPORT=`curl http://${API_SERVER}/nodeport`
+
+echo ${NODEPORT}
+
+# request kibana
+ANS = `curl ${KUBE_WORKER}:${NODEPORT}`
+
+echo ${ANS}
+
